be3fa38c2ed4a3b1e1907566aeb87c7ceca0ecd4
José Antonio Parejo Maestre <japarejo@gmail.com>
2019-01-05 13:47:39 +0100

    Merge origin/master
    Conflicts:
    	modules/SEDL-MetaModel/src/main/resources/metamodel/SEDL.notation
    	modules/SEDL-MetaModel/src/main/resources/metamodel/SEDL.uml

--- modules/SEDL-core/generated/es/us/isa/sedl/core/design/package-info.java
+++ modules/SEDL-core/generated/es/us/isa/sedl/core/design/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.01.05 a las 01:22:58 PM CET 
+// Generado el: 2019.01.05 a las 01:44:54 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://isa.us.es/sedl/core/design")
